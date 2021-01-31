d8a3c369d41ca9816343886a0792a5ff0a679590
alexandre <tconect.telecom@gmail.com>
2021-01-31 19:13:56 -0200

    alteraçoes

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,18 @@
     <img src="./assets/xandy.jpg" alt="xandy.jpg">
     <mainn />
     <cadastro/>
-    <contato/>
-    <router-view></router-view>
   </div>
 </template>
 <script>
 import cliente from "./components/cliente.vue";
 import mainn from "./components/mainn.vue";
 import cadastro from "./components/cadastro.vue";
-import contato from "./components/contato.vue";
-
 export default {
-  name: "App",
-  components: {
+    name: "App",
+    components: {
     cliente,
     mainn,
-    cadastro,
-    contato,
-    
+    cadastro
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import Vuelidate from 'vuelidate'
 
 
+
 Vue.config.productionTip = false
  
 Vue.use(Vuelidate)
 
+
 new Vue({
+  
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
+
