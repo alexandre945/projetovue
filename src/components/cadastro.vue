bb630475ac3ce19d25bb2e2e1c827a81c2a9cf1a
alexandre <tconect.telecom@gmail.com>
2021-02-15 11:54:25 -0200

    ajustando input

--- src/components/cadastro.vue
+++ src/components/cadastro.vue
@@ -6,7 +6,7 @@
               <form action="https://api.staticforms.xyz/submit" method="post">
                     <input v-model="nome" type="text" name="name" placeholder="seu nome:"><br>
                     <input v-model="email" type="text" name="email" placeholder="seu Email:" /><br>
-                    <textarea v-model="mensagem" name="message" placeholder="deixe,sua mensagem:"></textarea><br>
+                    <textarea v-model="mensagem" name="message" placeholder="sua mensagem:"></textarea><br>
                     <input type="text" name="honeypot" style="display:none">
                     <input type="hidden" name="accessKey" value="2811af00-0456-4c02-a5fb-adb2057f2630">
                     <input type="hidden" name="replyTo" value="xandylamabamg@hotmail.com">
